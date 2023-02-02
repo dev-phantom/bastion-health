5b5f7ddf310d659113104f9ff05dc6c2b2963536
favourite1975 <godsfavour1975@gmail.com>
2023-02-02 05:05:01 +0100

    feature: completed the health plan and fixed some errors

---
+++ src/data/Data.jsx
@@ -0,0 +1,26 @@
+export const plans =[
+    {
+        id:1,
+        bgColor: "#FFAC6A",
+        heading: "Shop Individual & Family Plan",
+        paragraph: "We have affordable and discounted premium to suit your budget. See further details below of the cover you can expect.",
+        btnlink: "#",
+        color: "#141414"
+    },
+    {
+        id:2,
+        bgColor: "#6F6FBA",
+        heading: "Small and Medium Enterprises",
+        paragraph: "When it comes to choosing a healthcare plan for your employees as a large corporate, we have got you covered. ",
+        btnlink: "#",
+        color: "#FFFFFF"
+    },
+    {
+        id:3,
+        bgColor: "rgba(14, 86, 63, 0.05)",
+        heading: "Get Corporate Health Plans",
+        paragraph: "Our affordable range of plans are designed to fit the unexpected healthcare cost of your employees and bring everyone peace of mind.",
+        btnlink: "#",
+        color: "#141414"
+    }
+]
