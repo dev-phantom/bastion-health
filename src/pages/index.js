3b0794b858aac914970b40d0b806e17d4b687cdb
favourite1975 <godsfavour1975@gmail.com>
2023-02-07 04:32:50 +0100

    fix: fixed fetching blog issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Homepage from '@/pages/system/home/Homepage'
-import Header from '@/components/common/Header'
 import Head from 'next/head'
 
 
@@ -12,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+
       <Homepage />
     </div>
   )
