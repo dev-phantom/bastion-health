9333201dc2847f96963249336d0ff080e3432ab0
dev-phantom <godsfavour1975@gmail.com>
2023-08-25 12:40:31 +0100

    feat: added responsiveness to the health plans

--- src/pages/system/HealthPlan/HealthPlan.jsx
+++ src/pages/system/HealthPlan/HealthPlan.jsx
@@ -1,21 +1,24 @@
 import Main from "@/components/organism/Main";
-import { Flex, Spacer, Box, Heading, ButtonGroup, Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Plans from "@/components/organism/Plans";
 import DefaultLayout from "@/layout/defaultLayout";
 
 const HealthPlan = () => {
-  return ( 
+  return (
     <DefaultLayout>
-      <Box px={{ base: 20 }} gap={8}>
-        <Main 
+      <Box px={{ base: 5, md: 20 }} gap={8}>
+        <Main
           headingText={"Get Affordable Health Plans with Bastion"}
-          paraText={"At Bastion, we have carefully designed an array of healthcare plans at different prices that adequately cater to your healthcare needs and those of your family, business and employees."}
+          paraText={
+            "At Bastion, we have carefully designed an array of healthcare plans at different prices that adequately cater to your healthcare needs and those of your family, business and employees."
+          }
           btnVisibility={"hidden"}
-          imgLink={"https://res.cloudinary.com/phantom1245/image/upload/v1675097789/bastion-health/HERO_gib2ih.png"}
+          imgLink={
+            "https://res.cloudinary.com/phantom1245/image/upload/v1675097789/bastion-health/HERO_gib2ih.png"
+          }
           imgAlt={"testin alt"}
         />
         <Plans />
-
       </Box>
     </DefaultLayout>
   );
